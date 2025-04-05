814a8aa126f76c2ca403e39769afac970ca5c2a5
tsh235 <tanyashustrova@yandex.ru>
2025-04-05 13:05:17 +0300

    подключен swiper и стилизован

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 import 'normalize.css';
 import './style.scss';
+import { Navigation, Thumbs } from 'swiper/modules';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+const sliderThimbs = new Swiper('.slider-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+new Swiper('.slider-main', {
+  spaceBetween: 10,
+  modules: [Navigation, Thumbs],
+  navigation: {
+    nextEl: '.slider__button_next',
+    prevEl: '.slider__button_prev',
+  },
+  thumbs: {
+    swiper: sliderThimbs,
+  },
+});
